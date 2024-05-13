4e8b52c5cfe7ad2b231bc8a79054abf9624bfbf3
Ali Idrees <aliidrees8600@gmail.com>
2024-05-13 21:17:28 +0500

    Added the name into the users, and displayed it.

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,5 @@ Rails.application.routes.draw do
   #profile routes
   get 'profile', to: 'profile#show', as: 'profile_show'
   root to: 'profile#show'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  #Set root for something
-  # Defines the root path route ("/")
-  # root "articles#index"
+
 end
