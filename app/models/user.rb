8fd2cd14e37d77fca0f8d49fc61471716e7280bf
Ali Idrees <aliidrees8600@gmail.com>
2024-05-14 13:28:18 +0500

    Added the User attribute based upon enum [admin teacher student] & displayed the Role during sign_up and also for update profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum Role: %i[ admin teacher student ]
 end
